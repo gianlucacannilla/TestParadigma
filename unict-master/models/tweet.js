5945cc55a641f9bca19cc0d6dc0c9b1b8a267dcd
Salvo-Sipala <47394117+Salvo-Sipala@users.noreply.github.com>
2019-11-27 17:06:13 +0100

    DB model modified

--- unict-master/models/tweet.js
+++ unict-master/models/tweet.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const tweetSchema = mongoose.Schema({
     _author: {type: mongoose.Schema.Types.ObjectId, required: true, index: true, ref: 'User'},
     tweet: {type: String, minlenght: 1, maxlenght: 280},
-    created_at: {type: Date, default: Date.now()}
+    created_at: {type: Date, default: Date.now()},
+    parent_tweet: {type: String}, //Story 1: id related with the "father" tweet
+    likes: {type: Number} //Story 2: number related to the likes of a tweet
+
 });
 
 const Tweet = mongoose.model('Tweet', tweetSchema);
--- unict-master/models/user.js
+++ unict-master/models/user.js
@@ -4,7 +4,9 @@ const userSchema = mongoose.Schema({
     name: String,
     surname: String,
     email: {type: String, required: true, unique: true},
-    password: {type: String, required: true}
+    password: {type: String, required: true},
+    favorites: {type: [String]}, //Story 3: array of strings for favorites
+    hashtags: {type: [String]} //Story 4: array of strings for hashtags
 });
 
 const User = mongoose.model('User', userSchema);
