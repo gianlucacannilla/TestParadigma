f912b3c23e52e611b85477ec4ea82a0dbab41038
gabriele-d23 <47394444+gabriele-d23@users.noreply.github.com>
2019-11-27 16:56:51 +0100

    update
    .1

--- unict-master/models/tweet.js
+++ unict-master/models/tweet.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const tweetSchema = mongoose.Schema({
     _author: {type: mongoose.Schema.Types.ObjectId, required: true, index: true, ref: 'User'},
+    parent_tweet: {type: String, minlenght: 1, maxlenght: 280},
     tweet: {type: String, minlenght: 1, maxlenght: 280},
     created_at: {type: Date, default: Date.now()}
 });
